74ae94451b70d66b4d78c989cd4729fdb765b48e
lavinvirmani2025-droid <lavinvirmani2025@gmail.com>
2025-12-22 20:48:17 +0530

    Remove Prisma completely and rely on Firebase backend

--- next-app/prisma/migrations/20251210165029_analytics_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "AnalyticsEvent" (
-    "id" SERIAL NOT NULL,
-    "type" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "AnalyticsEvent_pkey" PRIMARY KEY ("id")
-);
